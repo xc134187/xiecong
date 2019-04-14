377efc4fa1107968bcea4a1661d0cf5efd7482a2
wwbweibo <wwbweibo@outlook.com>
2019-04-14 15:48:55 +0800

    ...

---
+++ src/se/Model/Mapper/SubjectMapper.java
@@ -0,0 +1,10 @@
+/**
+ * @author: created by wwbweibo
+ * @version: 1.0
+ * @date: 2019/4/14
+ */
+
+package se.Model.Mapper;
+
+public interface SubjectMapper {
+}
---
+++ src/se/Model/Subject.java
@@ -0,0 +1,10 @@
+/**
+ * @author: created by wwbweibo
+ * @version: 1.0
+ * @date: 2019/4/14
+ */
+
+package se.Model;
+
+public class Subject {
+}
